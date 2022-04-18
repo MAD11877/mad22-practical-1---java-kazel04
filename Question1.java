41cafbb13ad3d3bbe2953682d6814d9034794fa1
Kazel <59406000+kazel04@users.noreply.github.com>
2022-04-18 08:15:12 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,11 +1,5 @@
-public class Question1
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
-  }
+class Main {  
+  public static void main(String args[]) { 
+    System.out.println("Hello, world!"); 
+  } 
 }
